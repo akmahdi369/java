bf5493dd5285af08af26e680bc0a80d0c70698e9
AK Mahdi <117555695+akmahdi369@users.noreply.github.com>
2022-12-22 11:51:29 +0600

    Add files via upload

---
+++ project.js
@@ -0,0 +1,15 @@
+document.write("Hello World");
+
+function mathMatics(Num1,Num2,Num3){
+    document. write("</br> The Addition:"+(Num1+Num2+Num3));
+    document. write("</br> The subtraction:"+(Num1-Num2-Num3));
+    document. write("</br> The multiplication:"+(Num1*Num2*Num3));
+    document. write("</br> The divition:"+((Num1)(Num2)(Num3)));
+}
+
+let N1=Number(prompt("Enter the number1"));
+let N2=Number(prompt("Enter the number2"));
+let N3=Number(prompt("Enter the number3"));
+
+mathMatics(N1,N2,N3)
+
